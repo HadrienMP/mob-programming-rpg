84192d064c061f82e82076d217f8590e0342ba51
hadrienmp <public@hadrienmp.fr>
2020-04-10 21:53:37 +0200

    Add pug and nodemon

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ let app = require("express")();
 let http = require('http').createServer(app);
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>');
+    res.render('index');
 });
 
+app.set('view engine', 'pug');
 let port = process.env.PORT || 3000;
 http.listen(port, () => console.log(`Server running on  http://0.0.0.0:${port}`));
