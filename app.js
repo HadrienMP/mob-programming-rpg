7fff31625057a16adabf597a678dd6d6cd0d1abb
hadrienmp <public@hadrienmp.fr>
2020-04-10 23:18:36 +0200

    Start adding content and styling

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-let app = require("express")();
+let express = require("express");
+let app = express();
 let http = require('http').createServer(app);
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.get('/', (req, res) => res.render('index', {title: "Règles"}));
+app.get('/rolesheets', (req, res) => res.render('rolesheets', {title: "Rôles"}));
 
 app.set('view engine', 'pug');
+app.use(express.static('public'));
 let port = process.env.PORT || 3000;
 http.listen(port, () => console.log(`Server running on  http://0.0.0.0:${port}`));
