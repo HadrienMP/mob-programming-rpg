ee5f1cad29acc9d5f9d6787d09a25152a2389804
hadrienmp <public@hadrienmp.fr>
2020-04-12 19:17:42 +0200

    Add a navigator sheet as model

--- app/routes.js
+++ app/routes.js
@@ -16,10 +16,28 @@ exports.setup = app => {
             ["traffic-cop"],
         ]
     }));
+
+    app.get('/role/:role', (req, res) => {
+        toHtml(`markdown/roles/${req.params.role}.md`,
+            html => {
+                return res.render('role', {
+                    title: req.params.role,
+                    description: html
+                });
+            },
+            err => {
+                console.log(err);
+                return res.sendStatus(404);
+            });
+    });
 }
 
-function toHtml(fileName, callback) {
+function toHtml(fileName, callback, errorCallback) {
     fs.readFile(fileName, "utf-8", (err, data) => {
-        if (!err) callback(markdown.render(data))
+        if (err) {
+            errorCallback(err);
+        } else {
+            callback(markdown.render(data))
+        }
     });
 }
---
+++ markdown/roles/navigator.md
@@ -0,0 +1,11 @@
+# Navigator
+__Brique par brique vous construisez dans l'obscurité.__
+Chaque pas vous rapproche du but en extrayant la sagesse du mob.
+
+## Gagnez un point d'expérience / 3
+Quand vous : 
+* demandez des idées
+* filtrez la sagesse du mob pour dicter exactement quoi taper au pilote
+* donnez uniquement votre intention au pilote en le laissant gérer les détails d'implémentation
+* Créez un test rouge, le faites passer et refactorez
+* Quand vous marquez un point d'un rôle niveau 1 que vous avez déjà fini
