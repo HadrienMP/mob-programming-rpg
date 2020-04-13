538d9666c84b8257c7ddd01106f5de8a2de1c3bb
hadrienmp <public@hadrienmp.fr>
2020-04-13 14:08:45 +0200

    Add the mob name in the url

--- app/app.js
+++ app/app.js
@@ -3,13 +3,12 @@ const app = express();
 const http = require('http').createServer(app);
 const routes = require("./routes");
 
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
 routes.setup(app);
 
-start();
+const port = process.env.PORT || 3000;
+http.listen(port, () => console.log(`Server running on  http://0.0.0.0:${port}`));
 
-function start() {
-    app.set('view engine', 'pug');
-    app.use(express.static('public'));
-    const port = process.env.PORT || 3000;
-    http.listen(port, () => console.log(`Server running on  http://0.0.0.0:${port}`));
-}
--- app/routes.js
+++ app/routes.js
@@ -2,17 +2,21 @@ const fs = require("fs");
 const markdown = require("jstransformer-markdown-it");
 
 exports.setup = app => {
-    app.get('/', (req, res) => res.redirect("/rules"));
+    app.get('/', (req, res) => res.redirect("/main/rules"));
 
     app.get('/home', (req, res) => res.render('home'));
-
-    app.get('/rules', (req, res) => {
-        toHtml("markdown/rules-rewrite.md",
-            html => res.render('rules', {title: "Règles", rules: html}));
+    app.post('/home', (req, res) => {
+        return res.redirect(`/${req.body.mob}/rules`);
     });
 
-    app.get('/roles', (req, res) => res.render('roles', {
+    app.get('/:mob/rules', (req, res) => {
+        toHtml("markdown/rules-rewrite.md",
+            html => res.render('rules', {title: "Règles", mob: req.params.mob, rules: html}));
+    });
+
+    app.get('/:mob/roles', (req, res) => res.render('roles', {
         title: "Rôles",
+        mob: req.params.mob,
         roles: [
             ["driver", "navigator", "mobber"],
             ["researcher", "sponsor", "rear-admiral"],
@@ -21,11 +25,12 @@ exports.setup = app => {
         ]
     }));
 
-    app.get('/role/:role', (req, res) => {
+    app.get('/:mob/role/:role', (req, res) => {
         toHtml(`markdown/roles/${req.params.role}.md`,
             html => {
                 return res.render('role', {
                     title: req.params.role,
+                    mob: req.params.mob,
                     description: html
                 });
             },
