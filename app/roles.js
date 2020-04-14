3727db3a5a9d8d46c99df81a70fedc32d2ce3970
hadrienmp <public@hadrienmp.fr>
2020-04-14 21:35:32 +0200

    Allow to win badges

---
+++ app/roles.js
@@ -0,0 +1,48 @@
+const mobs = {}
+
+exports.get = name => {
+    if (name in mobs) return mobs[name]
+    mobs[name] = [
+        {name: "driver", level: 1, count: 0},
+        {name: "navigator", level: 1, count: 0},
+        {name: "mobber", level: 1, count: 0},
+        {name: "researcher", level: 2, count: 0},
+        {name: "sponsor", level: 2, count: 0},
+        {name: "rear-admiral", level: 2, count: 0},
+        {name: "archivist", level: 3, count: 0},
+        {name: "automationist", level: 3, count: 0},
+        {name: "nose", level: 3, count: 0},
+        {name: "traffic-cop", level: 4, count: 0}
+    ];
+    return mobs[name];
+}
+exports.displayable = name => {
+    let notGrouped = this.get(name);
+    let grouped = notGrouped.groupBy(role => role.level);
+    return toDisplayableRoles(grouped);
+}
+
+Array.prototype.groupBy = function (getter) {
+    let grouped = {};
+    this.forEach(role => {
+        let list = grouped[getter(role)] || [];
+        list.push(role)
+        grouped[getter(role)] = list;
+    });
+    return grouped;
+}
+
+function toDisplayableRoles(grouped) {
+    let groupedList = []
+    for (const level in grouped) {
+        groupedList[level - 1] = grouped[level];
+    }
+    console.log(groupedList);
+    return groupedList;
+}
+
+exports.win = (mob, wonRole) => {
+    this.get(mob)
+        .find(role => role.name === wonRole)
+        .count ++;
+}
--- app/routes.js
+++ app/routes.js
@@ -1,29 +1,25 @@
 const fs = require("fs");
 const markdown = require("jstransformer-markdown-it");
+const roles = require("./roles");
 
 exports.setup = app => {
     app.get('/', (req, res) => res.redirect("/main/rules"));
 
     app.get('/home', (req, res) => res.render('home'));
-    app.post('/home', (req, res) => {
-        return res.redirect(`/${req.body.mob}/rules`);
-    });
+    app.post('/home', (req, res) => res.redirect(`/${req.body.mob}/rules`));
 
     app.get('/:mob/rules', (req, res) => {
         toHtml("markdown/rules-rewrite.md",
             html => res.render('rules', {title: "Règles", mob: req.params.mob, rules: html}));
     });
 
-    app.get('/:mob/roles', (req, res) => res.render('roles', {
-        title: "Rôles",
-        mob: req.params.mob,
-        roles: [
-            ["driver", "navigator", "mobber"],
-            ["researcher", "sponsor", "rear-admiral"],
-            ["archivist", "automationist", "nose"],
-            ["traffic-cop"],
-        ]
-    }));
+    app.get('/:mob/roles', (req, res) => {
+        return res.render('roles', {
+            title: "Rôles",
+            mob: req.params.mob,
+            roles: roles.displayable(req.params.mob)
+        });
+    });
 
     app.get('/:mob/role/:role', (req, res) => {
         toHtml(`markdown/roles/${req.params.role}.md`,
@@ -38,6 +34,11 @@ exports.setup = app => {
                 console.log(err);
                 return res.sendStatus(404);
             });
+    });
+
+    app.post('/:mob/role/:role/win', (req, res) => {
+        roles.win(req.params.mob, req.params.role);
+        res.redirect(`/${req.params.mob}/roles`);
     });
 }
 
